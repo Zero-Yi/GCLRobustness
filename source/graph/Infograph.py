8ab8a73305b8dffff1308e0254b3e35b8e6f22b8
rafiqkamel <rafiq.kamel@tum.de>
2023-08-01 16:05:45 +0200

    added infograph and dgi

---
+++ source/graph/Infograph.py
@@ -0,0 +1,182 @@
+import torch
+import os.path as osp
+import GCL.losses as L
+
+from torch import nn
+from tqdm import tqdm
+from torch.optim import Adam
+from GCL.eval import get_split, SVMEvaluator
+from GCL.models import SingleBranchContrast
+from torch_geometric.nn import GINConv, global_add_pool
+from torch_geometric.data import DataLoader
+import torch_geometric
+from torch_geometric.datasets import TUDataset
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from SVM_classify import *
+import pandas as pd
+import numpy as np
+
+def make_gin_conv(input_dim, out_dim):
+    return GINConv(nn.Sequential(nn.Linear(input_dim, out_dim), nn.ReLU(), nn.Linear(out_dim, out_dim)))
+
+
+class GConv(nn.Module):
+    def __init__(self, input_dim, hidden_dim, activation, num_layers):
+        super(GConv, self).__init__()
+        self.activation = activation()
+        self.layers = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+        for i in range(num_layers):
+            if i == 0:
+                self.layers.append(make_gin_conv(input_dim, hidden_dim))
+            else:
+                self.layers.append(make_gin_conv(hidden_dim, hidden_dim))
+            self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
+
+    def forward(self, x, edge_index, batch):
+        z = x
+        zs = []
+        for conv, bn in zip(self.layers, self.batch_norms):
+            z = conv(z, edge_index)
+            z = self.activation(z)
+            z = bn(z)
+            zs.append(z)
+        gs = [global_add_pool(z, batch) for z in zs]
+        z, g = [torch.cat(x, dim=1) for x in [zs, gs]]
+        return z, g
+
+
+class FC(nn.Module):
+    def __init__(self, hidden_dim):
+        super(FC, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
+        )
+        self.linear = nn.Linear(hidden_dim, hidden_dim)
+
+    def forward(self, x):
+        return self.fc(x) + self.linear(x)
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, encoder, local_fc, global_fc):
+        super(Encoder, self).__init__()
+        self.encoder = encoder
+        self.local_fc = local_fc
+        self.global_fc = global_fc
+
+    def forward(self, x, edge_index, batch):
+        z, g = self.encoder(x, edge_index, batch)
+        return z, g
+
+    def project(self, z, g):
+        return self.local_fc(z), self.global_fc(g)
+
+
+def train(encoder_model, contrast_model, dataloader, optimizer):
+    encoder_model.train()
+    epoch_loss = 0
+    for data in dataloader:
+        data = data.to('cuda')
+        optimizer.zero_grad()
+
+        if data.x is None:
+            num_nodes = data.batch.size(0)
+            data.x = torch.ones((num_nodes, 1), dtype=torch.float32, device=data.batch.device)
+
+        z, g = encoder_model(data.x, data.edge_index, data.batch)
+        z, g = encoder_model.project(z, g)
+        loss = contrast_model(h=z, g=g, batch=data.batch)
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+    return epoch_loss
+
+
+def test(encoder_model, dataloader):
+    encoder_model.eval()
+    x = []
+    y = []
+    for data in dataloader:
+        data = data.to('cuda')
+        if data.x is None:
+            num_nodes = data.batch.size(0)
+            data.x = torch.ones((num_nodes, 1), dtype=torch.float32, device=data.batch.device)
+        z, g = encoder_model(data.x, data.edge_index, data.batch)
+        x.append(g)
+        y.append(data.y)
+    x = torch.cat(x, dim=0)
+    y = torch.cat(y, dim=0)
+
+    split = get_split(num_samples=x.size()[0], train_ratio=0.8, test_ratio=0.1)
+    x_attack, y_attack = attack_test(encoder_model, dataloader)
+    result = SVMEvaluator(linear=True)(x, y, split)
+    attack_result = SVMEvaluator(linear=True)(x_attack, y_attack, split)
+    print(result)
+    return result, attack_result
+def attack_test(encoder_model, dataloader):
+    encoder_model.eval()
+    x = []
+    y = []
+    for data in dataloader:
+        data = data.to('cuda')
+        if data.x is None:
+            num_nodes = data.batch.size(0)
+            data.x = torch.ones((num_nodes, 1), dtype=torch.float32, device=data.batch.device)
+        updated_edge_index, _ = torch_geometric.utils.dropout_edge(data.edge_index, p = 0.1)
+        data.put_edge_index(updated_edge_index, layout='coo')
+        z, g = encoder_model(data.x, data.edge_index, data.batch)
+        x.append(g)
+        y.append(data.y)
+    x = torch.cat(x, dim=0)
+    y = torch.cat(y, dim=0)
+    return x,y
+
+
+def main(dataset_name):
+    device = torch.device('cuda')
+    path = osp.join(osp.expanduser('~'), 'datasets')
+    dataset = TUDataset(path, name=dataset_name)
+    dataloader = DataLoader(dataset, batch_size=8)
+    input_dim = max(dataset.num_features, 1)
+
+    gconv = GConv(input_dim=input_dim, hidden_dim=256, activation=torch.nn.ReLU, num_layers=2).to(device)
+    fc1 = FC(hidden_dim=256 * 2)
+    fc2 = FC(hidden_dim=256 * 2)
+    encoder_model = Encoder(encoder=gconv, local_fc=fc1, global_fc=fc2).to(device)
+    contrast_model = SingleBranchContrast(loss=L.JSD(), mode='G2L').to(device)
+
+    optimizer = Adam(encoder_model.parameters(), lr=0.001)
+
+    with tqdm(total=100, desc='(T)') as pbar:
+        for epoch in range(1, 101):
+            loss = train(encoder_model, contrast_model, dataloader, optimizer)
+            pbar.set_postfix({'loss': loss})
+            pbar.update()
+    #test_result = test(encoder_model, dataloader)
+    clf, correctly_classified_indices, x_test, y_test, accuracy = svm_build(dataloader, encoder_model)
+    att_acc = random_attack(encoder_model, clf, correctly_classified_indices, x_test, y_test, dataloader)
+    return accuracy, att_acc
+
+
+if __name__ == '__main__':
+    datasets = ['REDDIT-MULTI-5K', 'PROTEINS', 'REDDIT-BINARY', 'NCI1', 'DD']
+    for dataset in datasets:
+        accs = []
+        att_accs=[]
+        for _ in range(7):
+            acc, att_acc = main(dataset)
+            accs.append(acc)
+            att_accs.append(att_acc)
+        df = pd.DataFrame(np.array([accs, att_accs]).T, columns = ['accuracy', 'attack_accuracy'])
+        df.to_csv(dataset)
+        print(df.mean(axis=0))
+        print(df.std(axis=0))
---
+++ source/node/pure_dgi.py
@@ -0,0 +1,269 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn.models import DeepGraphInfomax, GCN
+from torch_geometric.datasets import CitationFull
+from torch_geometric.utils import get_laplacian, dropout_edge, add_random_edge
+from torch_geometric.contrib.nn import PRBCDAttack, GRBCDAttack
+
+import os.path as osp
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import GCL.augmentors as A
+import copy
+
+from aug.my_feature_masking import MyFeatureMasking
+from layer.readout import AvgReadout
+from layer.corrupt import FeatureShuffle
+from gin import LogReg, eval_encoder
+from attacker.greedy import Greedy
+from attacker.PGD import PGDAttack
+
+def disc(summary_aug, pos, neg, DGI):
+    pos_logits = DGI.discriminate(z = pos, summary = summary_aug, sigmoid = False)
+    neg_logits = DGI.discriminate(z = neg, summary = summary_aug, sigmoid = False)
+    return torch.cat((pos_logits.unsqueeze(1), neg_logits.unsqueeze(1)),1)
+
+class GCL_classifier(nn.Module):
+    def __init__(self, encoder, classifier):
+        super().__init__()
+        self.encoder = encoder
+        self.classifier = classifier
+    
+    def forward(self, x, edge_index, edge_weight=None, batch=None):
+        z = self.encoder(x, edge_index, edge_weight = edge_weight)
+        logits = self.classifier(z)
+        return logits
+
+def eval_encoder(model, y, x, edge_index, edge_weight=None, idx_test=None, device='cuda'):
+    '''
+    Workflow:
+        graph -> encoder => embeddings -> classifier => predictions
+    Return:
+        accuracy: accuracy on the evaluation set
+        correct_mask: to indicate the correctly classified nodes in the test set
+    '''
+    model.eval()
+
+    z = model.encoder(x = x, edge_index = edge_index, edge_weight = edge_weight)
+    if idx_test == None:
+        idx_test = torch.tensor(range(len(x))).to(device) # use all nodes as the test set
+    test_z = z[idx_test]
+    test_y = y[idx_test]
+
+    logits = model.classifier(test_z)
+    preds = torch.argmax(logits, dim=1)
+    correct_mask = (preds == test_y)
+    accuracy = torch.sum(correct_mask).float() / test_y.shape[0]
+
+    return accuracy, correct_mask
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description='dgi.py')
+    parser.add_argument('--dataset', type=str, default='Cora',
+                        help='Dataset')
+    parser.add_argument('--ratio', type=float, default='0.1',
+                        help='Perterbation ratio in augmentation. Default: 0.1')
+    parser.add_argument('--aug', type=str, default='edge',
+                        help='Type of augmentation. Edge, mask, node, or subgraph. Default: edge')
+    parser.add_argument('--PGD', type=bool, default=False,
+                        help='Whether apply PGD attack. Default: false')
+    parser.add_argument('--PRBCD', type=bool, default=False,
+                        help='Whether apply PRBCD attack. Default: false')
+    parser.add_argument('--GRBCD', type=bool, default=False,
+                        help='Whether apply GRBCD attack. Default: false')                    
+    parser.add_argument('--seed_split', type=int, default=42,
+                        help='Seed for the dataset split. Default: 42')
+    parser.add_argument('--seeds_encoder', nargs='+', type=int, default=[1,2,3,4,5],
+                        help='List of seeds for the encoder initialization. Default: [1,2,3,4,5]') 
+    parser.add_argument('--seeds_lc', nargs='+', type=int, default=[1,2,3,4,5],
+                        help='List of seeds for the classifier initialization. Default: [1,2,3,4,5]') 
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = arg_parse()
+    dataset_name = args.dataset
+    aug_pe = args.ratio
+    aug_type = args.aug
+    do_PGD_attack = args.PGD
+    do_PRBCD_attack = args.PRBCD
+    do_GRBCD_attack = args.GRBCD
+    seed_split = args.seed_split
+    seeds_encoder = args.seeds_encoder
+    seeds_lc = args.seeds_lc
+    hid_units = 512
+
+    # load dataset
+    device = torch.device('cuda')
+    path = osp.join(osp.expanduser('~'), 'datasets')
+    dataset = CitationFull(path, name=dataset_name)
+    # dataset = Planetoid(path, name=dataset_name)
+    assert len(dataset) == 1, "Expecting node classification on one huge graph"
+    
+    data = dataset[0]
+    x = data.x
+    y = data.y
+    edge_index = data.edge_index
+    n_features = dataset.num_features
+    num_classes = dataset.num_classes
+    num_nodes = x.shape[0]
+
+    lbl_1 = torch.ones(num_nodes).unsqueeze(1)
+    lbl_2 = torch.zeros(num_nodes).unsqueeze(1)
+    double_lbl = torch.cat((lbl_1, lbl_2), 1)
+
+    train_set, val_set, test_set = torch.utils.data.random_split(range(num_nodes), [0.8,0,0.2])
+    idx_train = torch.tensor(train_set.indices)
+    idx_val = torch.tensor(val_set.indices)
+    idx_test = torch.tensor(test_set.indices)
+
+
+
+
+
+    # model
+    torch.manual_seed(seeds_encoder[0])
+    torch.cuda.manual_seed(seeds_encoder[0])
+    encoder = GCN(in_channels=n_features, hidden_channels=hid_units, num_layers=1, act='prelu')
+    summary = AvgReadout()
+    corruption = FeatureShuffle()
+    model = DeepGraphInfomax(hidden_channels=hid_units, 
+                            encoder=encoder,
+                            summary=summary,
+                            corruption=corruption).to(device)
+    optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # to device
+    y = y.to(device)
+    x = x.to(device)
+    edge_index = edge_index.to(device)
+  
+
+    double_lbl = double_lbl.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+    b_xent = nn.BCEWithLogitsLoss()
+    cnt_wait = 0
+    best = 1e9
+    best_t = 0
+    # train
+    with tqdm(total=10000, desc='(T)') as pbar:
+        for epoch in range(10000):
+            model.train()
+
+            pz, nz, s = model(x, edge_index)
+
+            double_logits = disc(s, pz, nz, model)
+
+            loss = b_xent(double_logits, double_lbl)
+            if loss < best:
+                best = loss
+                best_t = epoch
+                cnt_wait = 0
+                torch.save(model.state_dict(), 'try.pkl')
+            else:
+                cnt_wait += 1
+
+            if cnt_wait == 20:
+                print('Early stopping!')
+                break
+
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
+
+            pbar.set_postfix({'loss': loss.item()})
+            pbar.update()
+
+    print('Loading {}th epoch'.format(best_t))
+    model.load_state_dict(torch.load('try.pkl'))
+
+    # test the model
+    model.eval()
+    model.requires_grad_(False)
+    z = model.encoder(x, edge_index)
+
+    train_z = z[idx_train]
+    train_y = y[idx_train]
+
+    xent = nn.CrossEntropyLoss()
+    accs_clean = []
+    accs_greedy = []
+    accs_PGD = []
+    accs_PRBCD = []
+    accs_GRBCD = []
+    for _ in tqdm(range(50)): # use 50 linear classifier
+        log = LogReg(hid_units, num_classes).to(device)
+        opt = torch.optim.Adam(log.parameters(), lr=0.01, weight_decay=0.0)
+
+        for _ in range(100):
+            log.train()
+            opt.zero_grad()
+
+            logits = log(train_z)
+            loss = xent(logits, train_y)
+            
+            loss.backward()
+            opt.step()
+
+        encoder_classifier = GCL_classifier(model.encoder, log)
+        encoder_classifier.eval()
+        # ++++++++++ clean ++++++++++++++++
+        acc, _ = eval_encoder(encoder_classifier, y, x, edge_index, idx_test=idx_test, device=device)
+        accs_clean.append(acc)
+
+        # ++++++++++ Greedy ++++++++++++++++
+        attack_ratio = 0.05
+        updated_edge_index, _ = dropout_edge(edge_index, p=attack_ratio, force_undirected=False) # Randomly drop edges
+        _, added_edges = add_random_edge(edge_index, p=attack_ratio, force_undirected=False) # Randomly add edges
+        updated_edge_index = torch.cat((updated_edge_index,added_edges), dim=1)
+
+        acc_greedy, _ = eval_encoder(encoder_classifier, y, x, updated_edge_index, idx_test=idx_test, device=device)
+        accs_greedy.append(acc_greedy)
+        # ++++++++++ Greedy over ++++++++++++++++
+
+        if do_PGD_attack == True:
+            # ++++++++++ PGD ++++++++++++++++
+            PGDattacker = PGDAttack(surrogate=encoder_classifier, device=device, epsilon=1e-4, log=False)
+            budget = int(edge_index.shape[1] * attack_ratio)
+            _, _, adv_edge_index = PGDattacker.attack_one_graph(x=x, edge_index=edge_index, batch=None, y=y, n_perturbations=budget)
+            
+            acc_PGD, _ = eval_encoder(encoder_classifier, y, x, adv_edge_index, idx_test=idx_test, device=device)
+            accs_PGD.append(acc_PGD)
+            # ++++++++++ PGD over ++++++++++++++++
+
+        if do_PRBCD_attack == True:
+            # ++++++++++ PRBCD ++++++++++++++++
+            PRBCDattacker = PRBCDAttack(model=encoder_classifier, block_size=250_000, log=False)
+            budget = int(edge_index.shape[1] * attack_ratio)
+            adv_edge_index, _ = PRBCDattacker.attack(x=x, edge_index=edge_index, labels=y, budget=budget)
+
+            acc_PRBCD, _ = eval_encoder(encoder_classifier, y, x, adv_edge_index, idx_test=idx_test, device=device)
+            accs_PRBCD.append(acc_PRBCD)
+            # ++++++++++ PRBCD over ++++++++++++++++
+        if do_GRBCD_attack == True:
+            # ++++++++++ GRBCD ++++++++++++++++
+            GRBCDattacker = GRBCDAttack(model=encoder_classifier, block_size=250_000, log=False)
+            budget = int(edge_index.shape[1] * attack_ratio)
+            adv_edge_index, _ = GRBCDattacker.attack(x=x, edge_index=edge_index, labels=y, budget=budget)
+
+            acc_GRBCD, _ = eval_encoder(encoder_classifier, y, x, adv_edge_index, idx_test=idx_test, device=device)
+            accs_GRBCD.append(acc_GRBCD)
+            # ++++++++++ GRBCD over ++++++++++++++++    
+    
+    accs_clean = torch.stack(accs_clean)
+    accs_greedy = torch.stack(accs_greedy)
+    print(f'(A): clean average accuracy={accs_clean.mean():.4f}, std={accs_clean.std():.4f}')
+    print(f'(A): greedy adversarial average accuracy={accs_greedy.mean():.4f}, std={accs_greedy.std():.4f}')
+    if do_PGD_attack == True:
+        accs_PGD = torch.stack(accs_PGD)
+        print(f'(A): PGD adversarial average accuracy={accs_PGD.mean():.4f}, std={accs_PGD.std():.4f}')
+    if do_PRBCD_attack == True:
+        accs_PRBCD = torch.stack(accs_PRBCD)
+        print(f'(A): PRBCD adversarial average accuracy={accs_PRBCD.mean():.4f}, std={accs_PRBCD.std():.4f}')
+    if do_GRBCD_attack == True:
+        accs_GRBCD = torch.stack(accs_GRBCD)
+        print(f'(A): GRBCD adversarial average accuracy={accs_GRBCD.mean():.4f}, std={accs_GRBCD.std():.4f}')    
